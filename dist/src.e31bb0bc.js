85be41cfb479eddd794264d1df96ac50775ca783
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2019-11-10 15:09:18 +1300

    yarn

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50507" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49945" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -324,4 +324,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/index.js.map
+//# sourceMappingURL=/src.e31bb0bc.js.map
